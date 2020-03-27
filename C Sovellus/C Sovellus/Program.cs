e20cc48f85bdd524c8334de99be55228aef9d3cb
sarilindellksw <sari.lindell@ksw.fi>
2020-03-27 10:25:11 +0200

    Lisätty

---
+++ C Sovellus/C Sovellus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sovellus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
